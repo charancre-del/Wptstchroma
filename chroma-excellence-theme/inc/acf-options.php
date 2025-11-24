a7434dd1c9c7e0ab3661c3de31cb56dc1950f9eb
charancre-del <charancre@gmail.com>
2025-11-23 21:03:43 -0500

    Remove reliance on ACF options

--- chroma-excellence-theme/inc/acf-options.php
+++ chroma-excellence-theme/inc/acf-options.php
@@ -12,48 +12,96 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Register ACF Options Page
+ * Global configuration pulled from wp_options/theme_mods to avoid ACF reliance.
  */
-if ( function_exists( 'acf_add_options_page' ) ) {
-	acf_add_options_page( array(
-		'page_title' => __( 'Chroma Settings', 'chroma-excellence' ),
-		'menu_title' => __( 'Chroma Settings', 'chroma-excellence' ),
-		'menu_slug'  => 'chroma-settings',
-		'capability' => 'edit_posts',
-		'icon_url'   => 'dashicons-admin-settings',
-		'position'   => 2,
-	) );
+function chroma_get_global_settings() {
+	static $settings = null;
+
+	if ( null !== $settings ) {
+		return $settings;
+	}
+
+	$defaults = array(
+		'phone'                   => '',
+		'email'                   => '',
+		'tour_email'              => '',
+		'address'                 => '',
+		'city'                    => '',
+		'state'                   => 'GA',
+		'zip'                     => '',
+		'facebook_url'            => '',
+		'instagram_url'           => '',
+		'linkedin_url'            => '',
+		'seo_default_title'       => '',
+		'seo_default_description' => '',
+		'logo_id'                 => 0,
+		'logo_url'                => '',
+	);
+
+	$option_settings = get_option( 'chroma_global_settings', array() );
+
+	// Allow overriding with Theme Mods if set individually.
+	$theme_mod_overrides = array(
+		'phone'         => get_theme_mod( 'chroma_global_phone' ),
+		'email'         => get_theme_mod( 'chroma_global_email' ),
+		'tour_email'    => get_theme_mod( 'chroma_global_tour_email' ),
+		'address'       => get_theme_mod( 'chroma_global_address' ),
+		'city'          => get_theme_mod( 'chroma_global_city' ),
+		'state'         => get_theme_mod( 'chroma_global_state' ),
+		'zip'           => get_theme_mod( 'chroma_global_zip' ),
+		'facebook_url'  => get_theme_mod( 'chroma_global_facebook_url' ),
+		'instagram_url' => get_theme_mod( 'chroma_global_instagram_url' ),
+		'linkedin_url'  => get_theme_mod( 'chroma_global_linkedin_url' ),
+		'logo_id'       => get_theme_mod( 'chroma_global_logo_id' ),
+		'logo_url'      => get_theme_mod( 'chroma_global_logo_url' ),
+	);
+
+	$settings = wp_parse_args( array_filter( $theme_mod_overrides ), $option_settings );
+	$settings = wp_parse_args( $settings, $defaults );
+
+	/**
+	 * Filters global theme settings.
+	 *
+	 * @param array $settings Theme configuration settings.
+	 */
+	$settings = apply_filters( 'chroma_global_settings', $settings );
+
+	return $settings;
 }
 
 /**
  * Global Phone Helper
  */
 function chroma_global_phone() {
-	return get_field( 'global_phone', 'option' ) ?: '';
+	$settings = chroma_get_global_settings();
+	return $settings['phone'] ?: '';
 }
 
 /**
  * Global Email Helper
  */
 function chroma_global_email() {
-	return get_field( 'global_email', 'option' ) ?: '';
+	$settings = chroma_get_global_settings();
+	return $settings['email'] ?: '';
 }
 
 /**
  * Global Tour Email Helper
  */
 function chroma_global_tour_email() {
-	return get_field( 'global_tour_email', 'option' ) ?: chroma_global_email();
+	$settings = chroma_get_global_settings();
+	return $settings['tour_email'] ?: chroma_global_email();
 }
 
 /**
  * Global Full Address Helper
  */
 function chroma_global_full_address() {
-	$address = get_field( 'global_address', 'option' );
-	$city    = get_field( 'global_city', 'option' );
-	$state   = get_field( 'global_state', 'option' );
-	$zip     = get_field( 'global_zip', 'option' );
+	$settings = chroma_get_global_settings();
+	$address  = $settings['address'];
+	$city     = $settings['city'];
+	$state    = $settings['state'];
+	$zip      = $settings['zip'];
 
 	if ( ! $address ) {
 		return '';
@@ -72,33 +120,60 @@ function chroma_global_full_address() {
  * Global Facebook URL
  */
 function chroma_global_facebook_url() {
-	return get_field( 'global_facebook_url', 'option' ) ?: '';
+	$settings = chroma_get_global_settings();
+	return $settings['facebook_url'] ?: '';
 }
 
 /**
  * Global Instagram URL
  */
 function chroma_global_instagram_url() {
-	return get_field( 'global_instagram_url', 'option' ) ?: '';
+	$settings = chroma_get_global_settings();
+	return $settings['instagram_url'] ?: '';
 }
 
 /**
  * Global LinkedIn URL
  */
 function chroma_global_linkedin_url() {
-	return get_field( 'global_linkedin_url', 'option' ) ?: '';
+	$settings = chroma_get_global_settings();
+	return $settings['linkedin_url'] ?: '';
 }
 
 /**
  * Global SEO Default Title
  */
 function chroma_global_seo_default_title() {
-	return get_field( 'global_seo_default_title', 'option' ) ?: get_bloginfo( 'name' );
+	$settings = chroma_get_global_settings();
+	return $settings['seo_default_title'] ?: get_bloginfo( 'name' );
 }
 
 /**
  * Global SEO Default Description
  */
 function chroma_global_seo_default_description() {
-	return get_field( 'global_seo_default_description', 'option' ) ?: get_bloginfo( 'description' );
+	$settings = chroma_get_global_settings();
+	return $settings['seo_default_description'] ?: get_bloginfo( 'description' );
+}
+
+/**
+ * Global logo helper using Theme Mod or stored settings.
+ */
+function chroma_global_logo_url() {
+	$settings = chroma_get_global_settings();
+
+	if ( ! empty( $settings['logo_url'] ) ) {
+		return $settings['logo_url'];
+	}
+
+	$logo_id = absint( $settings['logo_id'] );
+	if ( $logo_id ) {
+		$logo = wp_get_attachment_image_url( $logo_id, 'full' );
+		if ( $logo ) {
+			return $logo;
+		}
+	}
+
+	$custom_logo_id = get_theme_mod( 'custom_logo' );
+	return $custom_logo_id ? wp_get_attachment_image_url( $custom_logo_id, 'full' ) : '';
 }
