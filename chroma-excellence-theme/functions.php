13faf92307b1cbcd9819ec778e857fa4bacd49d6
charancre-del <charancre@gmail.com>
2026-01-08 04:25:08 -0500

    chore: Clean up functions.php header and enforce error suppression

--- chroma-excellence-theme/functions.php
+++ chroma-excellence-theme/functions.php
@@ -10,19 +10,13 @@
 
 // Exit if accessed directly
 if (!defined('ABSPATH')) {
-
     exit;
 }
 
-// Enable error reporting for debugging
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Exit if accessed directly
-if (!defined('ABSPATH')) {
-
-    exit;
+// Disable error reporting for production
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
 }
 
 /**
