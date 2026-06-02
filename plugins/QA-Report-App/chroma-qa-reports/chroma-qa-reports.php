be66072806cd1c42fe59f8bbb781774fca3fba77
charancre-del <charancre@gmail.com>
2026-06-02 02:05:49 -0400

    Backfill QA school tiers on upgrade

--- plugins/QA-Report-App/chroma-qa-reports/chroma-qa-reports.php
+++ plugins/QA-Report-App/chroma-qa-reports/chroma-qa-reports.php
@@ -3,7 +3,7 @@
  * Plugin Name: Chroma QA Reports
  * Plugin URI: https://chromaearlylearning.com/qa-reports
  * Description: Quality Assurance Report Management System for Chroma Early Learning Academy schools.
- * Version: 1.4.0
+ * Version: 1.4.1
  * Author: Chroma Early Learning Academy
  * Author URI: https://chromaearlylearning.com
  * License: GPL v2 or later
@@ -27,7 +27,7 @@ if (!defined('ABSPATH')) {
 if (!defined('CQA_DEBUG')) {
     define('CQA_DEBUG', defined('WP_DEBUG') && WP_DEBUG);
 }
-define('CQA_VERSION', '1.4.0');
+define('CQA_VERSION', '1.4.1');
 define('CQA_PLUGIN_FILE', __FILE__);
 define('CQA_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('CQA_PLUGIN_URL', plugin_dir_url(__FILE__));
